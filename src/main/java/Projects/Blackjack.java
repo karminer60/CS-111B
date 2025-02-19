0b95be6a9f396acc9c5a62d99e943429374f3685
karminer60 <karminer60@gmail.com>
2025-02-19 15:53:11 -0800

    adds Blackjack project

---
+++ src/main/java/Projects/Blackjack.java
@@ -0,0 +1,57 @@
+package Projects;
+
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+	    /* Pseudocode:
+
+        - choose two random cards to start with
+
+        - initialize a card total variable to zero and then add the value of the two cards that were randomly selected
+        - print out the total value
+        - create a while loop that checks whether the total is greater than or equal to 21
+        - in the while loop, if the total equals 21, print "blackjack" and exit. If it's greater than 21, print "busted" and exit
+        - Otherwise, print: "Do you want more cards?"
+        - if they say no to more cards, exit the while loop*/
+
+        //Choose two random cards to start with
+        Random rand = new Random();
+        int card1 = rand.nextInt(10 + 1);
+        int card2 = rand.nextInt(10 + 1);
+
+
+        //Create a varible to receive user input
+        int cardTotal = card1 + card2;
+
+
+        Scanner scn = new Scanner(System.in);
+        //create a while loop that checks whether the total is greater than or equal to 21
+        while(true){
+            //Print out cardTotal
+            System.out.println("The card total is: " + cardTotal);
+            if(cardTotal == 21){
+                System.out.println("Blackjack!");
+                break;
+            }
+            if(cardTotal > 21){
+                System.out.println("Busted!");
+                break;
+            }
+
+            //get input from the user
+            System.out.println("Do you want more cards?");
+            String userInput = scn.nextLine();
+            if(userInput.charAt(0) == 'y'){
+                int card = rand.nextInt(10 + 1);
+                cardTotal = cardTotal + card;
+            }else{
+                break;
+            }
+
+
+        }
+        System.out.print("The card total is: " + cardTotal);
+    }
+}
---
+++ src/main/java/com/example/cs111b/Cs111BApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs111b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs111BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs111BApplication.class, args);
+    }
+
+}
